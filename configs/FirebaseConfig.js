eb365be70296aec465450350069c311cfe3d90c2
David Joyce <davidajoyce141@gmail.com>
2024-08-12 21:22:40 +1000

    clean up

--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPzQkongvLCXhHxNLJPsMppHZxta3iYdY",
-  authDomain: "study-2c2ea.firebaseapp.com",
-  projectId: "study-2c2ea",
-  storageBucket: "study-2c2ea.appspot.com",
-  messagingSenderId: "424929637589",
-  appId: "1:424929637589:web:468a6ecc42c426f6b65e5c"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
